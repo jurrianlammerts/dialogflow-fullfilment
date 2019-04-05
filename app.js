fc5041788b4b7e43afdf5e660272ed06bceb1dd8
Jurrian Lammerts <jurrian@chainels.com>
2019-04-05 12:15:33 +0200

    Firebase connection succeded

--- app.js
+++ app.js
@@ -1,18 +1,45 @@
-const express = require('express')
-const { WebhookClient } = require('dialogflow-fulfillment')
-const app = express()
+const admin = require('firebase-admin');
+const serviceAccount = require('./serviceAccountKey.json');
+const express = require('express');
+const { WebhookClient } = require('dialogflow-fulfillment');
+const app = express();
 
-app.get('/', (req, res) => res.send('online'))
+// initialise DB connection
+admin.initializeApp({
+  credential: admin.credential.applicationDefault(),
+  databaseURL: 'https://voix-233614.firebaseio.com'
+});
+
+app.get('/', (req, res) => res.send('online'));
 app.post('/dialogflow', express.json(), (req, res) => {
-  const agent = new WebhookClient({ request: req, response: res })
+  const agent = new WebhookClient({ request: req, response: res });
 
-  function welcome () {
-    agent.add('Welcome to my agent!')
+  function handleName(agent) {
+    const name = agent.parameters.name;
+    const userId = 1;
+
+    if (name) {
+      agent.add(`Thank you ${name}!`);
+
+      return admin
+        .database()
+        .ref('/users/' + userId)
+        .set({
+          name: name
+        })
+        .then(snapshot => {
+          // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
+          console.log('database write succesfull: ' + snapshot.ref.toString());
+        });
+    } else {
+      agent.add(`Oops something went wrong... Please try again.`);
+    }
   }
 
-  let intentMap = new Map()
-  intentMap.set('Default Welcome Intent', welcome)
-  agent.handleRequest(intentMap)
-})
+  // Run the proper function handler based on the matched Dialogflow intent name
+  let intentMap = new Map();
+  intentMap.set('AskName', handleName);
+  agent.handleRequest(intentMap);
+});
 
-module.exports = app
+module.exports = app;
