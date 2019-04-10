165d2393dd5643608b0298b6c7a6138e2d9243ee
Marvin Holleman <marvinholleman@MacBook-Pro-van-Marvin-2.local>
2019-04-10 21:41:38 +0200

    edit functions for relocation

--- app.js
+++ app.js
@@ -30,11 +30,107 @@ app.post('/dialogflow', express.json(), (req, res) => {
       });
   }
 
+  function handleName(agent) {
+    const name = agent.parameters.name;
+    const userId = 1;
+
+    if (name) {
+      agent.add(`Thank you ${name}!`);
+
+      return admin
+        .database()
+        .ref('users/' + userId)
+        .set({
+          name: name
+        })
+        .then(snapshot => {
+          // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
+          console.log('database write succesfull: ' + snapshot.ref.toString());
+        });
+    } else {
+      agent.add(`Oops something went wrong... Please try again.`);
+    }
+  }
+
+
+  function handleAdress(agent) {
+    const adress = agent.parameters.adress;
+    const userId = 1;
+
+    if (adress) {
+      agent.add(`Thank you ${adress}!`);
+
+      return admin
+        .database()
+        .ref('users/' + userId)
+        .update({
+          adress: adress
+        })
+        .then(snapshot => {
+          // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
+          console.log('database write succesfull: ' + snapshot.ref.toString());
+        });
+    } else {
+      agent.add(`Oops something went wrong... Please try again.`);
+    }
+  }
+
+  function handleBuyOrRent(agent) {
+    const buying = agent.parameters.buying;
+    const userId = 1;
+
+    if (buying) {
+      agent.add(`Thank you ${buying}!`);
+
+      return admin
+        .database()
+        .ref('users/' + userId)
+        .update({
+          buyingOrRenting: buying
+        })
+        .then(snapshot => {
+          // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
+          console.log('database write succesfull: ' + snapshot.ref.toString());
+        });
+    } else {
+      agent.add(`Oops something went wrong... Please try again.`);
+    }
+  }
+
+  function handlePermit(agent) {
+    const permit = agent.parameters.permit;
+    const userId = 1;
+
+    if (permit) {
+      agent.add(`Thank you ${permit}!`);
+
+      return admin
+        .database()
+        .ref('users/' + userId)
+        .update({
+          permit: permit
+        })
+        .then(snapshot => {
+          // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
+          console.log('database write succesfull: ' + snapshot.ref.toString());
+        });
+    } else {
+      agent.add(`Oops something went wrong... Please try again.`);
+    }
+  }
+
   // Run the proper function handler based on the matched Dialogflow intent name
   let intentMap = new Map();
 
   // request passport
   intentMap.set('AskForData', saveData);
+
+  intentMap.set('Relocate-yes-askname', handleName);
+  intentMap.set('Relocate-yes-askname-askAdress', handleAdress);
+  intentMap.set('Relocate-yes-askname-askAdress-buy-or-rent', handleBuyOrRent);
+  intentMap.set('Relocate-yes-askname-askAdress-buy-or-rent-parking', handlePermit);
+  // intentMap.set('confirm', confirmRequest);
+
   agent.handleRequest(intentMap);
 });
 
